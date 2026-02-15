a32add0bdec2e486e80fb3fed06b19ddf5cb4a70
XIAOBAO LU <2806153115@qq.com>
2026-02-15 14:09:10 -0800

    Add frontend as regular folder

---
+++ social-ai_frontend/src/constants.js
@@ -0,0 +1,7 @@
+export const TOKEN_KEY = "token";
+export const BASE_URL = "https://helical-theater-440302-i3.de.r.appspot.com";
+export const SEARCH_KEY = {
+  all: 0,
+  keyword: 1,
+  user: 2,
+};
