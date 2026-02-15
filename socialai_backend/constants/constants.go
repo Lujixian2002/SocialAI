c648f7e1c003455ef6927ad7ba410447e558c899
XIAOBAO LU <2806153115@qq.com>
2026-02-15 13:49:18 -0800

    init

---
+++ socialai_backend/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://35.185.146.10:9200"
+    ES_USERNAME = "ljx"
+    ES_PASSWORD = "ljx021112"
+	GCS_BUCKET = "social-ai"
+)
